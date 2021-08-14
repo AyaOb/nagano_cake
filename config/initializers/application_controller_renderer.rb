ce723fb99baa56756222e8f30e3c0cf8edbd450e
EC2 Default User <ec2-user@ip-172-31-60-188.ec2.internal>
2021-08-14 07:22:06 +0000

    [Add] admin/items: show view & _item_form view

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0f4316cd61acf7c0680de366fe52bedd1b7eb0ecde28f3d85e803c0e3712904d16aae75f93fc84d32f41b75d02043bd559a274b198cb2677c1283c7815bfdf3b'
