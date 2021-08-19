aa8505fa407a56ffdf16d1d12b8e1f0c16bb26a5
EC2 Default User <ec2-user@ip-172-31-60-188.ec2.internal>
2021-08-19 10:54:21 +0000

    [Add]public/confirm controller & view

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,10 +17,14 @@ class Public::CustomersController < ApplicationController
   end
 
   def quit
+    @customer = current_customer
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
   end
 
   private
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_active)
   end
 end
